656973481ac61d602226250887c12c5127e747df
Aleksy <aslepaugo@gmail.com>
2023-06-02 20:21:20 +0200

    add keyboard

--- vk_bot.py
+++ vk_bot.py
@@ -6,6 +6,7 @@ import vk_api as vk
 
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 from logger_handler import TelegramBotHandler
 
@@ -13,10 +14,18 @@ logger = logging.getLogger(__name__)
 
 
 def reply(event, vk_api):
+    keyboard = VkKeyboard(one_time=True)
+
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button('Мой счет', color=VkKeyboardColor.PRIMARY)
+
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text if event.text else "None",
-        random_id=random.randint(1, 1000)
+        random_id=random.randint(1, 1000),
+        keyboard=keyboard.get_keyboard()
     )
 
 
